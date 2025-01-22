b06f85f27476215a0ea5004e3eeee6ceef885997
penso996 <penso.andrea@outlook.it>
2025-01-22 17:08:45 +0100

    script done

---
+++ script/script.js
@@ -0,0 +1,32 @@
+// selezioniamo l'elemento di output
+const emailList = document.getElementById('email-list');
+
+// settiamo il numero di email da generare e l'endpoint
+const numEmails = 10;
+const endpoint = 'https://flynn.boolean.careers/exercises/api/random/mail';
+
+// facciamo partire le richieste verso l'API
+for (let i = 0; i < numEmails; i++) {
+    axios.get(endpoint)
+        .then(apiEmail => {
+            // codice da eseguire in caso di SUCCESSO
+            // creo la costante contenente l'email generata
+            const email = apiEmail.data.response;
+
+            // creiamo un nuovo elemento <li> con l'email ricevuta
+            const emailLi = document.createElement('li');
+            emailLi.textContent = email;
+
+            // aggiungiamo l'elemento alla lista
+            emailList.appendChild(emailLi);
+        })
+        .catch(error => {
+            // codice da eseguire in caso di ERRORE
+            // creiamo un nuovo elemento <li> con il messaggio di errore
+            const emailLiError = document.createElement('li');
+            emailLiError.textContent = "API non caricato correttamente";
+
+            // aggiungiamo l'elemento alla lista
+            emailList.appendChild(emailLiError);
+        });
+}
